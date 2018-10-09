091858e28d1bc47fa06c42c8c58025882c823369
toFindMore <17855824057@163.com>
2018-10-09 09:53:46 +0800

    8.10

---
+++ src/main/java/sorting/SortingBlade.java
@@ -0,0 +1,30 @@
+package sorting;
+
+public class SortingBlade {
+    public static void sort(Comparable[] a) {
+        System.out.println("OK");
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i,int j) {
+        Comparable t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+
+    public static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if(less(a[i],a[i-1])) return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/test/java/sorting/SortingBladeTest.java
@@ -0,0 +1,17 @@
+package sorting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class SortingBladeTest {
+    @Test
+    public void NumTest() {
+        String[] a = {"1","2","0","4"};
+        SortingBlade sortingBlade = new SortingBlade();
+        sortingBlade.sort(a);
+
+
+    }
+}
