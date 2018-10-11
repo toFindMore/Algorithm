54e26d5ac7ad6448850140675e1d20f6b5656b68
toFindMore <17855824057@163.com>
2018-10-11 13:58:32 +0800

    10.11 自底向上底归并排序

--- src/main/java/sorting/MergeBUSort.java
+++ src/main/java/sorting/MergeBUSort.java
@@ -6,7 +6,6 @@ public class MergeBUSort extends SortingBlade {
     public void sort(Comparable[] a) {
         aux = new Comparable[a.length];
         for (int step = 1; step < a.length; step = step+step) {
-            System.out.println(step);
             for (int i = 0; i < a.length; i += 2*step) {
                 merge( a,i,Math.min(a.length-1,step+i-1) ,Math.min(a.length-1,i+2*step-1) );
             }
